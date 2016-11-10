05bc0f9ee951aace573f08c7512009de97d06a9f
Marcus Kainth <marcuskainth@live.com>
2016-11-10 15:17:49 +0000

    Replace map with tile
     * As map is a function in python

--- GameOfLife.py
+++ GameOfLife.py
@@ -1,5 +1,4 @@
-map_size = 64
-
+tiles_size = 64
 
 class cell:
     def __init__(self, location, alive=False):
@@ -9,61 +8,61 @@ class cell:
 
 class Rules:
     def rule(self, cell_):  # if alive
-        for i in xrange(map_size):
-            for j in xrange(map_size):
-                c = self.neighbourscounter(self, map[i][j])
+        for i in xrange(tiles_size):
+            for j in xrange(tiles_size):
+                c = self.neighbourscounter(self, tile[i][j])
 
-                if map[i][j].alive:
+                if tile[i][j].alive:
                     if c != 2 or c != 3:
-                        map[i][j].alive = False
+                        tile[i][j].alive = False
                     else:
-                        map[i][j].alive = True
+                        tile[i][j].alive = True
 
                 else:
                     if c == 3:
-                        map[i][j].alive = True
+                        tile[i][j].alive = True
 
     def neighbourscounter(self, cell_):  # Return the number of Neighbours alive#
         c = 0
         cell_loc = cell.location
         try:
-            if map[abs(cell_loc[0] - 1)][abs(cell_loc[1] - 1)].alive:
+            if tile[abs(cell_loc[0] - 1)][abs(cell_loc[1] - 1)].alive:
                 c += 1
         except Exception:
             pass
 
         try:
-            if map[abs(cell_loc[0] - 1)][abs(cell_loc[1])].alive:
+            if tile[abs(cell_loc[0] - 1)][abs(cell_loc[1])].alive:
                 c += 1
         except Exception:
             pass
         try:
-            if map[abs(cell_loc[0])][abs(cell_loc[1] - 1)].alive:
+            if tile[abs(cell_loc[0])][abs(cell_loc[1] - 1)].alive:
                 c += 1
         except Exception:
             pass
         try:
-            if map[abs(cell_loc[0] + 1)][abs(cell_loc[1] - 1)].alive:
+            if tile[abs(cell_loc[0] + 1)][abs(cell_loc[1] - 1)].alive:
                 c += 1
         except Exception:
             pass
         try:
-            if map[abs(cell_loc[0] + 1)][abs(cell_loc[1])].alive:
+            if tile[abs(cell_loc[0] + 1)][abs(cell_loc[1])].alive:
                 c += 1
         except Exception:
             pass
         try:
-            if map[abs(cell_loc[0] - 1)][abs(cell_loc[1] + 1)].alive:
+            if tile[abs(cell_loc[0] - 1)][abs(cell_loc[1] + 1)].alive:
                 c += 1
         except Exception:
             pass
         try:
-            if map[abs(cell_loc[0])][abs(cell_loc[1] + 1)].alive:
+            if tile[abs(cell_loc[0])][abs(cell_loc[1] + 1)].alive:
                 c += 1
         except Exception:
             pass
         try:
-            if map[abs(cell_loc[0] + 1)][abs(cell_loc[1] + 1)].alive:
+            if tile[abs(cell_loc[0] + 1)][abs(cell_loc[1] + 1)].alive:
                 c += 1
         except Exception:
             pass
@@ -71,8 +70,8 @@ class Rules:
         return c
 
 
-Map = []
-for i in xrange(map_size):
-    map.append([])
-    for g in xrange(map_size):
-        map[i].insert(g, cell((i, g)))
+tile = []
+for i in xrange(tiles_size):
+    tile.append([])
+    for g in xrange(tiles_size):
+        tile[i].insert(g, cell((i, g)))
