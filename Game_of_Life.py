76546482e23c88d473e22d0f6f35222cf90afdd0
JoaquinDF <joaquindf@usal.es>
2016-11-14 16:00:40 +0000

    Gun Pattern

--- Game_of_Life.py
+++ Game_of_Life.py
@@ -43,8 +43,63 @@ x = raw_input('Set an action or Pattern - Enter or rng for random cells distribu
 if x == '' or x == 'rng':
     # populate grid with random on/off - more off than on
     grid = np.random.choice(cells, gridSize * gridSize, p=[0.2, 0.8]).reshape(gridSize, gridSize)
-else:
-    print('Not implemented yed!')
+elif x== 'gun':
+    grid = np.zeros((gridSize, gridSize), dtype=bool)
+    grid[1][5]=True
+    grid[1][6]=True
+    grid[2][5]=True
+    grid[2][6]=True
+    grid[11][5]=True
+    grid[11][6]=True
+    grid[11][7]=True
+    grid[12][4]=True
+    grid[12][8]=True
+    grid[13][3]=True
+    grid[13][9]=True
+    grid[14][3]=True
+    grid[14][9]=True
+    grid[15][6]=True
+    grid[16][4]=True
+    grid[16][8]=True
+    grid[17][5]=True
+    grid[17][6]=True
+    grid[17][7]=True
+    grid[18][6]=True
+    grid[21][3]=True
+    grid[21][4]=True
+    grid[21][5]=True
+    grid[22][3]=True
+    grid[22][4]=True
+    grid[22][5]=True
+    grid[23][2]=True
+    grid[23][6]=True
+    grid[25][1]=True
+    grid[25][2]=True
+    grid[25][6]=True
+    grid[25][7]=True
+    grid[35][3]=True
+    grid[35][4]=True
+    grid[36][3]=True
+    grid[36][4]=True
+    grid[35][22]=True
+    grid[35][23]=True
+    grid[35][25]=True
+    grid[36][22]=True
+    grid[36][23]=True
+    grid[36][25]=True
+    grid[36][26]=True
+    grid[36][27]=True
+    grid[37][28]=True
+    grid[38][22]=True
+    grid[38][23]=True
+    grid[38][25]=True
+    grid[38][26]=True
+    grid[38][27]=True
+    grid[39][23]=True
+    grid[39][25]=True
+    grid[40][23]=True
+    grid[40][25]=True
+    grid[41][24]=True
 
 def update(data):
     if pause == False:
@@ -68,3 +123,4 @@ fig.canvas.mpl_connect('button_press_event', onClick)
 mat = ax.matshow(grid)
 ani = animation.FuncAnimation(fig, update, frames=60, interval=150, save_count=50)
 plt.show()
+
